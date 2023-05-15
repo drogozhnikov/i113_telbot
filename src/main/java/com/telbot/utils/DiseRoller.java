8579add895b7bedb2cdc599f4e58106a232fd369
drogozhnikov <DitarRogozhnikov@gmail.com>
2023-05-15 18:28:48 +0300

    Updated: DiceRoller now cant roll zero value

--- src/main/java/com/telbot/utils/DiseRoller.java
+++ src/main/java/com/telbot/utils/DiseRoller.java
@@ -2,9 +2,10 @@ package com.telbot.utils;
 
 public class DiseRoller {
 
-    public String roll(int count){
+    public String roll(int count) {
         count++;
-        int roll = (int) (Math.random()*count);
+        int min = 1;
+        int roll = (int) (Math.random() * (count - min) + min);
         return String.valueOf(roll);
     }
 }
