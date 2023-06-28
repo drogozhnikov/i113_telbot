7531297f7f7f8a4f21c1069f24dcad52dff1323f
drogozhnikov <DitarRogozhnikov@gmail.com>
2023-06-28 14:57:19 +0300

    Updated: properties

--- src/main/java/com/telbot/TelegramBotApplication.java
+++ src/main/java/com/telbot/TelegramBotApplication.java
@@ -3,7 +3,6 @@ package com.telbot;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -12,8 +11,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @OpenAPIDefinition(info = @Info(title = "TelegramBot"))
 public class TelegramBotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TelegramBotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotApplication.class, args);
+    }
 
 }
